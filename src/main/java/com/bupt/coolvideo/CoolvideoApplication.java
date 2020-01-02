38de7a8d0d76164377a0647eba98ae954606cf03
huangxin <572780350@qq.com>
2020-01-02 21:59:53 +0800

    First Commit

---
+++ src/main/java/com/bupt/coolvideo/CoolvideoApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.coolvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolvideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolvideoApplication.class, args);
+    }
+
+}
